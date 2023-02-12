3f366a3b8af954a2de02ecedd6b672f576f78ee7
Dmitry <56763340+dmitrypavlikov@users.noreply.github.com>
2023-02-13 00:12:48 +0300

    2 simple servos was add here

--- FeeTech_addr_control.ino
+++ FeeTech_addr_control.ino
@@ -1,8 +1,10 @@
+#include <Servo.h>
 #include "DxlMaster.h"
+
+
 const uint8_t LW_id = 1;      //Left Wheel id
 const uint8_t RW_id = 2;      //Right Wheel id
-const uint8_t BODY_id = 11;   //Body Servo id
-const uint8_t HAND_id = 12;   //Hand Servo id
+//Servos does not have ID
 
 int SPD_L = 0;                //Initial speed of left wheel
 int SPD_R = 0;                //Initial speed of right wheel
@@ -19,8 +21,8 @@ const long unsigned int baudrate = 1000000;
 
 DynamixelDevice LW(LW_id);
 DynamixelDevice RW(RW_id);
-DynamixelDevice BODY(BODY_id);
-DynamixelDevice HAND(HAND_id);
+Servo BODY;
+Servo HAND;
 
 void setup() 
 { Serial.begin(9600);         //Отладочный Serial
@@ -31,35 +33,27 @@ void setup()
 
   LW.init();
   RW.init();
-  BODY.init();
-  HAND.init();
+  BODY.attach(12);          //PWM pin for BODY servo
+  HAND.attach(13);          //PWM pin for HAND servo
   
   LW.write(0x28, 1);        //ENABLE_TORQUE
   RW.write(0x28, 1);        //ENABLE_TORQUE
-  BODY.write(0x28, 1);      //ENABLE_TORQUE
-  HAND.write(0x28, 1);      //ENABLE_TORQUE
   
   LW.write(0x1A, 0);        //DYN_ADDRESS_CW_LIMIT
   RW.write(0x1A, 0);        //DYN_ADDRESS_CW_LIMIT
-  BODY.write(0x1A, 0);      //DYN_ADDRESS_CW_LIMIT
-  HAND.write(0x1A, 0);      //DYN_ADDRESS_CW_LIMIT
   
   LW.write(0x1B, 0);        //DYN_ADDRESS_CCW_LIMIT
   RW.write(0x1B, 0);        //DYN_ADDRESS_CCW_LIMIT
-  BODY.write(0x1B, 0);      //DYN_ADDRESS_CCW_LIMIT
-  HAND.write(0x1B, 0);      //DYN_ADDRESS_CCW_LIMIT
   
   LW.write(0x31, 1);        //wheel mode
   RW.write(0x31, 1);        //wheel mode
-  BODY.write(0x31, 0);      //servo mode
-  HAND.write(0x31, 0);      //servo mode
 }
 
 void loop() {
   LW.read(0x3A, present_SPD_L);         //Read vel of left wheel 
   RW.read(0x3A, present_SPD_R);         //Read vel of right wheel
-  BODY.read(0x38, present_BODY_ANGLE);  //Read angle of body
-  HAND.read(0x38, present_HAND_ANGLE);  //Read angle of hand
+  // Чтение данных с серв тела и рук не происходит. 
+  //Отправляем то, что и получили, или самое начальное положение.
   
   char mail[20] = {};                   //Буфер для чтения сообщения 
   if (Serial3.available()){             //Проверка соединения по Rx Tx
@@ -67,8 +61,24 @@ void loop() {
     Serial.println(mail);                       //Дублирование пакета в отладочный Serial 
     
   }
-  
-  
+
+  //Дальше дробим сообщение на числа и задаём их в сервы
+  /* 
+   *  
+   * Дробим сообщение на числа  
+   *  
+   */
+
+  //Запись положений в сервы
+  /*
+   * 
+   * Запись положений в сервы
+   * 
+   */
+
+
+
+  //Напоминалка о работе со скоростью (wheelMode) и чтению
   /*device.write(0x2E, makeSpeed(5000)); //DYN_ADDRESS_GOAL_SPEED
   device.read(0x38, pos);
   device.read(0x3A, vel);
