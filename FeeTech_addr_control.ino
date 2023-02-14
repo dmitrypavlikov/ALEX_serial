a1bc7e3169afab6f0b03887ef2285040132ae404
Dmitry <56763340+dmitrypavlikov@users.noreply.github.com>
2023-02-14 17:13:42 +0300

    Version until discussing a mail format

--- FeeTech_addr_control.ino
+++ FeeTech_addr_control.ino
@@ -24,6 +24,65 @@ DynamixelDevice RW(RW_id);
 Servo BODY;
 Servo HAND;
 
+String sendMail(){
+
+  LW.read(0x3A, present_SPD_L);         //Read vel of left wheel (0x38 - позиция / 0x3A - скорость)
+  RW.read(0x3A, present_SPD_R);         //Read vel of right wheel (0x38 - позиция / 0x3A - скорость)
+  present_BODY_ANGLE = BODY.read();     //Read angle of body servo
+  present_HAND_ANGLE = HAND.read();     //Read andle of hand servo
+  
+  //Формат сообщения (+XXXX,+XXXX,XXX,XXX.)
+  //                 (LW,RW,BODY,HAND.)
+  return String(present_SPD_L) + ',' + String(present_SPD_R) + ',' 
+         + String(present_BODY_ANGLE) + ',' + String(present_HAND_ANGLE) + '.';
+}
+
+void readMail(){
+  char mail[20] = {};                           //Буфер для чтения сообщения (+XXXX,+XXXX,XXX,XXX.)
+  if (Serial3.available()){                     //Проверка почтового ящика по Rx Tx
+    Serial3.readBytesUntil('.', mail, 20);      //Чтение пакета до символа '.'
+    Serial.println(mail);                       //Дублирование полученного пакета в отладочный Serial 
+    
+    //Обнуление всех переменных перед записью. Пока не уверен, нужно ли...
+    SPD_L = 0;
+    SPD_R = 0;
+    BODY_ANGLE = 0;
+    HAND_ANGLE = 0;                     
+
+    String num;
+    short int counter = 0;
+    for(int i = 0; i < sizeof(mail); i++){
+      if((mail[i] != ',')&&(mail[i] != ' ')){
+        num += mail[i];}
+      else{ 
+        switch(counter){
+          case 0:
+            SPD_L = num.toInt();
+            break;
+          case 1:
+            SPD_R = num.toInt();
+            break;
+          case 2:
+            BODY_ANGLE = num.toInt();
+            break;
+          /*case 3:
+            HAND_ANGLE = num.toInt();
+            Serial.print(HAND_ANGLE);
+            break;*/}
+        counter++;
+        num = "";    
+      }
+    }
+    HAND_ANGLE = num.toInt();
+
+    //Прямая передача данных на моторы возможна тут
+    //Прямая передача данных на сервы возможна тут
+    
+  }
+}
+
+
+
 void setup() 
 { Serial.begin(9600);         //Отладочный Serial
   Serial3.begin(9600);        //Общение с NanoPi
@@ -33,50 +92,41 @@ void setup()
 
   LW.init();
   RW.init();
-  BODY.attach(12);          //PWM pin for BODY servo
-  HAND.attach(13);          //PWM pin for HAND servo
+  BODY.attach(12);            //PWM pin for BODY servo
+  HAND.attach(13);            //PWM pin for HAND servo
   
-  LW.write(0x28, 1);        //ENABLE_TORQUE
-  RW.write(0x28, 1);        //ENABLE_TORQUE
+  LW.write(0x28, 1);          //ENABLE_TORQUE
+  RW.write(0x28, 1);          //ENABLE_TORQUE
   
-  LW.write(0x1A, 0);        //DYN_ADDRESS_CW_LIMIT
-  RW.write(0x1A, 0);        //DYN_ADDRESS_CW_LIMIT
+  LW.write(0x1A, 0);          //DYN_ADDRESS_CW_LIMIT
+  RW.write(0x1A, 0);          //DYN_ADDRESS_CW_LIMIT
   
-  LW.write(0x1B, 0);        //DYN_ADDRESS_CCW_LIMIT
-  RW.write(0x1B, 0);        //DYN_ADDRESS_CCW_LIMIT
+  LW.write(0x1B, 0);          //DYN_ADDRESS_CCW_LIMIT
+  RW.write(0x1B, 0);          //DYN_ADDRESS_CCW_LIMIT
   
-  LW.write(0x31, 1);        //wheel mode
-  RW.write(0x31, 1);        //wheel mode
+  LW.write(0x31, 1);          //wheel mode
+  RW.write(0x31, 1);          //wheel mode
+
+  //Исходные положения и значения скоростей
+  LW.write(0x2E, SPD_L);         //
+  RW.write(0x2E, SPD_R);         //
+  BODY.write(BODY_ANGLE);        //Важно првильно закрепить сервы (сервы ведут отсчёт от нуля)
+  HAND.write(HAND_ANGLE);        //Важно првильно закрепить сервы (сервы ведут отсчёт от нуля) 
 }
 
-void loop() {
-  LW.read(0x3A, present_SPD_L);         //Read vel of left wheel 
-  RW.read(0x3A, present_SPD_R);         //Read vel of right wheel
-  // Чтение данных с серв тела и рук не происходит. 
-  //Отправляем то, что и получили, или самое начальное положение.
+void loop() {  
   
-  char mail[20] = {};                   //Буфер для чтения сообщения 
-  if (Serial3.available()){             //Проверка соединения по Rx Tx
-    Serial3.readBytesUntil('\n', mail, 20);     //Чтение пакета до символа '\n'
-    Serial.println(mail);                       //Дублирование пакета в отладочный Serial 
+  //Serial.println(sendMail());    //Отправка пакета информации в отлидочный Serial
+  Serial3.println(sendMail());   //Отправка пакета информации на NanoPi 
+
+  readMail();                    //Приём пакета данных от NanoPi (и пряма передача на моторы и сервы)
+  
+  
+  
     
-  }
+  
 
-  //Дальше дробим сообщение на числа и задаём их в сервы
-  /* 
-   *  
-   * Дробим сообщение на числа  
-   *  
-   */
-
-  //Запись положений в сервы
-  /*
-   * 
-   * Запись положений в сервы
-   * 
-   */
-
-
+  
 
   //Напоминалка о работе со скоростью (wheelMode) и чтению
   /*device.write(0x2E, makeSpeed(5000)); //DYN_ADDRESS_GOAL_SPEED
